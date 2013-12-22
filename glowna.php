6c62c736d6a5fe6d940638504f788a0f4c283d3a
Łukasz Szeremeta <l.szeremeta.dev@gmail.com>
2013-12-22 21:26:32 +0100

    Brak przycisku Główna jeżeli wejście z aplikacji
    Przycisk Główna nie jest pokazywany jeżeli użytkownik wchodzi z
    aplikacji.
    
    Przycisk jest natomiast dodawany do kodu wynikowego w przypadku wejścia
    z przeglądarki (za pomocą index.php lub glowna.php).

--- glowna.php
+++ glowna.php
@@ -7,18 +7,22 @@ $template->setOutputMode(PHPTAL::HTML5);
 
 /* Zawartość specyficzna dla platform - Android/przeglądarka */
 	
+			 /* - Wskaźnik stanu połączenia z siecią; nie wyświetlany w aplikacji
+				- Przycisk "Główna"; nie wyświetlany w aplikacji */
+				
 	// isset powinno być szybsze niż porównywanie wartości.
 	if (isset($_SESSION['client'])) {
 		$template->offline = '';
+		$template->glowna = '';
 	}
 	else {
 		$template->offline =
 			'        <!-- offline indicator - wskaźnik stanu połączenia z siecią (online/offline) -->
 	<script src=offline.min.js></script>
 	<link rel=stylesheet href=offline-indicator.css>';
+		$template->glowna = '<a href=glowna.php data-icon=home data-iconpos=left>Główna</a>';
 	}
-
-
+	
 
 $template->nazwa = "Instytut Informatyki";
 $template->pelny_tytul = 'mobiUwB - Instytut Informatyki';
