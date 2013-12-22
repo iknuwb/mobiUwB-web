064d8a6f25eb5c278ce594010335934078799073
Łukasz Szeremeta <l.szeremeta.dev@gmail.com>
2013-12-22 20:11:50 +0100

    Ukrycie wskaźnika połączenia z siecią jeżeli wejście z aplikacji
    Ukrycie wskaźnika połączenia z siecią jeżeli użytkownik korzysta z
    aplikacji.

--- glowna.php
+++ glowna.php
@@ -5,6 +5,21 @@ session_start();
 $template = new PHPTAL('template_index.html');
 $template->setOutputMode(PHPTAL::HTML5);
 
+/* Zawartość specyficzna dla platform - Android/przeglądarka */
+	
+	// isset powinno być szybsze niż porównywanie wartości.
+	if (isset($_SESSION['client'])) {
+		$template->offline = '';
+	}
+	else {
+		$template->offline =
+			'        <!-- offline indicator - wskaźnik stanu połączenia z siecią (online/offline) -->
+	<script src=offline.min.js></script>
+	<link rel=stylesheet href=offline-indicator.css>';
+	}
+
+
+
 $template->nazwa = "Instytut Informatyki";
 $template->pelny_tytul = 'mobiUwB - Instytut Informatyki';
 
