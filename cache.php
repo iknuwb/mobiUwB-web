38a02d210120e2650fbb4c3c694659ebcc31d075
Łukasz Szeremeta <l.szeremeta.dev@gmail.com>
2014-03-15 20:11:15 +0100

    Dynamiczne generowanie HTML5 cache manifestu
    HTML5 cache manifest jest teraz generowany dynamicznie. Pozwala to na
    wymuszenie aktualizacji cache aplikacji i wczytywanie nowych danych co
    np. godzinę.
    
    Gdy nastąpi aktualizacja cache aplikacji, strona jest automatycznie
    odświeżana (skrypt w JavaScript), aby od razu pokazać nowe dane.

---
+++ cache.php
@@ -0,0 +1,27 @@
+<?php
+header('Content-type: text/cache-manifest');
+
+// pobieramy aktualny czas (UNIXowy znacznik czasu)
+$czas = time();
+
+// wczytujemy znacznik czasu zapisany w ostatnia_aktualizacja.txt
+$ostatnia_aktualizacja = file_get_contents('ostatnia_aktualizacja.txt');
+
+// wczytujemy szablon manifestu
+$manifest_template = file_get_contents('template_cache.manifest');
+
+// aktualizujemy plik manifestu co godzinę (60*60*1)
+if ($ostatnia_aktualizacja + 3600 < $czas){
+
+	// generujemy nowy manifest
+	echo "CACHE MANIFEST\n# $czas\n".$manifest_template;
+	
+	// zapisujemy nowy czas aktualizacji (timestamp)
+	file_put_contents('ostatnia_aktualizacja.txt', $czas);
+}
+else{
+	// wczytujemy "stary" manifest
+	echo "CACHE MANIFEST\n# $ostatnia_aktualizacja\n".$manifest_template;
+}
+
+?>
