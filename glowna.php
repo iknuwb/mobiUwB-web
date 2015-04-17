f0860002cdbf3e7e7c503f8591a1528a0f7cf187
Łukasz Szeremeta <l.szeremeta.dev@gmail.com>
2015-04-17 22:57:36 +0200

    Update legacy support files
    * change redirect path
    * add annotation

--- glowna.php
+++ glowna.php
@@ -9,16 +9,12 @@
  */
 
 
+setcookie("client", "", time() - 3600);
+
 error_reporting(E_ALL);
 ini_set('display_errors', 0); // wyłączone wyświetlanie błędów (ustawienie dla produkcji)
-// wejście z aplikacji mobilnej (Android)
-setcookie('client', 'Android');
-
-// użytkownik używa wersji starej - ciasteczko jest teraz ustawiane przez aplikację mobilną, która otwiera glowna.php zamiast android.php
-setcookie('wersja', 'stara');
 
 $host = $_SERVER['HTTP_HOST'];
 $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 header("Location: http://$host$uri/");
 exit;
-
