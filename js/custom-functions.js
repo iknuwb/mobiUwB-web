364cadebccecec3bdbd326fef11106b940448879
Łukasz Szeremeta <l.szeremeta.dev@gmail.com>
2015-04-30 20:24:26 +0200

    Clean up functions
    * move all possible functions to js/custom-functions.js
    * remove old, unused anti-spam function

--- js/custom-functions.js
+++ js/custom-functions.js
@@ -1,7 +1,27 @@
-// antybot
-$(function () {
-    $('#poczta').each(function () {
-        this.href = this.href.replace('http://', 'mailto:sekretariat@adres')
-        this.href = this.href.replace('adres', '').replace('/', '');
-    });
+// fancybox - lightbox
+$(document).ready(function () {
+    $('a[rel="lightbox"]').fancybox();
 });
+
+//ochrona przed spamem
+$( document ).ready(function() {
+  $('#poczta').each(function() {
+    var email = this.href.replace('(malpa)', '@').replace(/\(kropka\)/g, '.');
+    this.href = email;
+    $('#poczta').html(email.replace('mailto:', ''));  
+  });
+});
+
+// Sprawdza czy plik manifestu został zaktualizowany, jeżeli tak, strona jest odświeżana
+window.addEventListener('load', function(e) {
+
+  window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      // Przeglądarka odświeżyła cache. Przeładowujemy stronę.
+      window.location.reload();
+    } else {
+      // Manifest nie został zmieniony.
+    }
+  }, false);
+
+}, false);
