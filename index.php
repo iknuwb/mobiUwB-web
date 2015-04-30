474f1cac4f4a2375fd20329d10766bf2c18ba25b
Łukasz Szeremeta <l.szeremeta.dev@gmail.com>
2015-04-30 18:45:37 +0200

    Better comments (cleaning and rewriting)

--- index.php
+++ index.php
@@ -38,7 +38,6 @@ switch ($_GET['place']) {
 
 /*
  * Załadowanie pliku konfiguracyjnego
- * 
  */
 
 if (file_exists('config.xml')) {
@@ -47,8 +46,10 @@ if (file_exists('config.xml')) {
     exit('Nie mozna otworzyc pliku config.xml.');
 }
 
-// ---------------- AUTORZY ----------------
-// klasa Autor
+/*
+ * Utworzenie klas
+ */
+
 class Autor {
 
     public $imie;
@@ -66,13 +67,7 @@ foreach ($config->autorzy->autor as $autor) {
     $autorzy[] = new Autor($autor);
 }
 
-// $template->autorzy = $autorzy;
-// -------------------------------------------
-// --------------- OPIEKUNOWIE ---------------
-
 class Opiekun extends Autor {
-
-    //public $imie;
 
     function __construct($imie) {
         $this->imie = $imie . ' (opiekun, autor)';
@@ -86,10 +81,6 @@ $opiekun = array();
 foreach ($config->opiekun as $opiekun) {
     $opiekunowie[] = new Opiekun($opiekun);
 }
-
-//$template->opiekunowie = $opiekunowie;
-// -------------------------------------------
-// ------------------ PLANY ------------------
 
 class Plan {
 
@@ -117,13 +108,7 @@ foreach ($config->jednostka[$p]->plany->drugi_stopien->plan as $plan_IIst) {
     $plany_IIst[] = new Plan($plan_IIst->link, $plan_IIst->opis);
 }
 
-// $template->plany_Ist = $plany_Ist;
-// $template->plany_IIst = $plany_IIst;
-// -------------------------------------------
-
 $template->apiUrl = $api_url = $config->jednostka[$p]->apiUrl;
-
-// ----------------- SEKCJE -----------------
 
 class Sekcja {
 
@@ -163,7 +148,6 @@ class Elementy {
 $sekcje = array();
 
 foreach ($config->jednostka[$p]->sekcje->sekcja as $sekcja) {
-    /* UWAGA UWAGA UWAGA */
     $json = file_get_contents("$api_url" . $sekcja->id_sekcji . "/6/0"); //limit 6 z offsetem 0
     $json_ile = file_get_contents("$api_url" . $sekcja->id_sekcji); //wczytujemy wszystko aby sprawdzic ile jest elementow tablicy
     $s = json_decode($json, true);
@@ -184,11 +168,11 @@ foreach ($config->jednostka[$p]->sekcje->sekcja as $sekcja) {
         $dzien = $dataarr[2];
         $godzina = $czasarr[0];
         $minuty = $czasarr[1];
-        $sekundy = $czasarr[2]; // nie uzywam teraz
+        $sekundy = $czasarr[2]; // obecnie nie używane
         $polskadata = $dzien . '.' . $miesiac . '.' . $rok . ' (' . $godzina . ':' . $minuty . ')';
         $elementy[] = new Elementy($s[$i]['tresc'], $s[$i]['tytul'], $polskadata);
     }
-    //$elementy[] = new Elementy("bla", "Bla", "bla");//usunac
+
     if ($sekcja['licznik'] != 'tak') {
         $sekcje[] = new Sekcja($sekcja->id_sekcji, $sekcja->tytul_sekcji, "<a href=\"#$sekcja->id_sekcji\" class=$sekcja->id_sekcji>$sekcja->tytul_sekcji</a>", $elementy, $ile, $wiecej);
     } else {
@@ -196,14 +180,7 @@ foreach ($config->jednostka[$p]->sekcje->sekcja as $sekcja) {
     }
 }
 
-// $template->sekcje = $sekcje;
-// -------------------------------------------	
-// ------------ SEKCJE STATYCZNE -------------
-
 class SekcjaStatyczna extends Sekcja {
-
-    //public $id_sekcji;
-    //public $tytul_sekcji;
 
     function __construct($id_sekcji, $tytul_sekcji) {
         $this->id_sekcji = $id_sekcji;
@@ -219,10 +196,11 @@ foreach ($config->jednostka[$p]->sekcje_statyczne->sekcja_statyczna as $sekcja_s
     $sekcje_statyczne[] = new SekcjaStatyczna($sekcja_statyczna->id_sekcji, $sekcja_statyczna->tytul_sekcji);
 }
 
-// $template->sekcje_statyczne = $sekcje_statyczne;
-// -------------------------------------------	
 
-/* Wczytanie danych z pliku konfiguracyjnego */
+
+/*
+ * Ustawianie danych pobranych z pliku konfiguracyjnego
+ */
 
 $template->opiekunowie = $opiekunowie;
 $template->autorzy = $autorzy;
@@ -231,8 +209,6 @@ $template->plany_Ist = $plany_Ist;
 $template->plany_IIst = $plany_IIst;
 $template->sekcje = $sekcje;
 $template->sekcje_statyczne = $sekcje_statyczne;
-
-// --
 
 $template->nazwa = $config->jednostka[$p]->nazwa;
 $template->pelny_tytul = $config->jednostka[$p]->pelny_tytul;
@@ -244,13 +220,13 @@ $template->logo_srednie = $config->logo_srednie;  // 64 x 64
 $template->logo_duze = $config->logo_duze;    // 128 x 128
 
 $template->logo_jednostki = $config->jednostka[$p]->logo; // opcjonalne
-//$template->apiUrl = $config->jednostka[$p]->apiUrl; // jest nad wczytywaniem sekcji
-// mapa
+
+/* mapa */
 $template->tytul = $config->jednostka[$p]->mapa->tytul;
 $template->dlugosc = $config->jednostka[$p]->mapa->wspolrzedne->dlugosc;
 $template->szerokosc = $config->jednostka[$p]->mapa->wspolrzedne->szerokosc;
 
-// adres
+/* adres */
 $template->kod = $config->jednostka[$p]->adres->kod;
 $template->miasto = $config->jednostka[$p]->adres->miasto;
 $template->ulica = $config->jednostka[$p]->adres->ulica;
@@ -270,12 +246,13 @@ $template->tel2 = $config->jednostka[$p]->tel2; // opcjonalne
 $template->fax = $config->jednostka[$p]->fax; // opcjonalne
 
 
-/* Zawartość specyficzna dla platform - Android/przeglądarka */
-
 /*
-  - MobiUwB w belce tytułowej na stronie głównej; nie wyświetlana w aplikacji
-  - Wskaźnik stanu połączenia z siecią; nie wyświetlany w aplikacji
-  - Przycisk "Główna"; nie wyświetlany w aplikacji
+ * Zawartość specyficzna dla platform - Android/przeglądarka
+ * 
+ * - MobiUwB w belce tytułowej na stronie głównej; nie wyświetlana w aplikacji
+ * - Wskaźnik stanu połączenia z siecią; nie wyświetlany w aplikacji
+ * - Przycisk "Główna"; nie wyświetlany w aplikacji
+ * 
  */
 
 $template->nowa_wersja = '';
@@ -302,6 +279,10 @@ if (isset($_COOKIE['client'])) {
     $template->glowna = "<a href=./?place=$place data-icon=home data-iconpos=left>Główna</a>";
 }
 
+
+/*
+ * Odpalenie szablonu
+ */
 
 try {
     echo $template->execute();
