b1571d176557dd76f167e19d20b924945d87fd5b
Łukasz Szeremeta <l.szeremeta.dev@gmail.com>
2016-10-06 21:03:22 +0200

    Cache update refactoring

--- cache.php
+++ cache.php
@@ -5,24 +5,24 @@ error_reporting(E_ALL);
 ini_set('display_errors', 0); // wyłączone wyświetlanie błędów (ustawienie dla produkcji)
 
 // pobieramy aktualny czas (UNIXowy znacznik czasu)
-$czas = time();
+$current_time = time();
 
-// wczytujemy znacznik czasu zapisany w ostatnia_aktualizacja.txt
-$ostatnia_aktualizacja = file_get_contents('ostatnia_aktualizacja.txt');
+// wczytujemy znacznik czasu zapisany w last_cache_update
+$last_update = file_get_contents('last_cache_update');
 
 // wczytujemy szablon manifestu
 $manifest_template = file_get_contents('template_cache.manifest');
 
 // aktualizujemy plik manifestu co godzinę (60*60*1)
-if ($ostatnia_aktualizacja + 3600 < $czas){
+if ($last_update + 3600 < $current_time){
 
 	// generujemy nowy manifest
-	echo "CACHE MANIFEST\n# $czas\n".$manifest_template;
+	echo "CACHE MANIFEST\n# $current_time\n".$manifest_template;
 	
 	// zapisujemy nowy czas aktualizacji (timestamp)
-	file_put_contents('ostatnia_aktualizacja.txt', $czas);
+	file_put_contents('last_cache_update', $current_time);
 }
 else{
 	// wczytujemy "stary" manifest
-	echo "CACHE MANIFEST\n# $ostatnia_aktualizacja\n".$manifest_template;
+	echo "CACHE MANIFEST\n# $last_update\n".$manifest_template;
 }
